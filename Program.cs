240de396a7772db4a7096324d8f130b158dc0f70
atitiya <gift.22110@gmail.com>
2023-03-13 16:44:53 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ class Program {   
+    static void Main(string[] args){
+        int day,time;
+        int num_breakfast =5, num_weekend =2, num_coffee =3;
+        int.TryParse(Console.ReadLine(), out day);
+        int.TryParse(Console.ReadLine(), out time);
+
+        while(true){
+            String menu = Console.ReadLine ();
+            if(menu == "End"){
+                break;
+            }
+            if((menu == "Breakfast Set" && num_breakfast == 0)
+             ||(menu == "Weekend Set" && num_weekend == 0)
+             ||(menu == "Coffee" && num_coffee == 0)){
+                Console.WriteLine("Sorry your order is out of stock");
+             }
+            else if (menu == "Breackfast Set"&& day > 11){
+                num_breakfast -=1;
+            }
+            else if(menu == "Breakfast Set" && time > 11){
+                Console.WriteLine("Sorry your order is not available");
+            }
+            else if(menu == "Weekend Set" && day <= 5){
+                Console.WriteLine("Sorry your order is not available");
+            }
+            else if(menu == "Weekend Set" && day > 5){
+                num_coffee -=1;
+            }
+            else {
+                Console.WriteLine("Please enter a valid menu");
+            }
+        }
+    }
+}
